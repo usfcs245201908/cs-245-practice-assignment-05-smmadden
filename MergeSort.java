0c00499d38f3bd78456d8b51dfbc4e93c799351c
smmadden <smmadden10@gmail.com>
2019-09-20 15:25:38 -0700

    completed merge sort (tested)

--- MergeSort.java
+++ MergeSort.java
@@ -5,7 +5,7 @@ public class MergeSort implements SortingAlgorithm {
     public void sort(int [] a){
         if(a.length > 1){
             int[] left = Arrays.copyOfRange(a, 0, a.length/2);
-            int[] right = Arrays.copyOfRange(a,a.length/2 + 1, a.length);
+            int[] right = Arrays.copyOfRange(a,a.length/2, a.length);
             sort(left);
             sort(right);
             merge(a, left, right);
@@ -14,18 +14,19 @@ public class MergeSort implements SortingAlgorithm {
 
     private void merge(int[] a, int[] left, int[] right){
         int k=0, i=0, j=0;
-        while(i < left.length && j < right.length){ // checking to see if elements left
+        while(i < left.length && j < right.length){
             if(left[i] <= right[j]){
                 a[k++] = left[i++];
             } else {
                 a[k++] = right[j++];
             }
-            while(i< left.length){
-                a[k++] = left[i++];
-            }
-            while(j< right.length){
-                a[k++] = right[j++];
-            }
+        }
+        while(i< left.length){
+            a[k++] = left[i++];
+        }
+        while(j< right.length){
+            a[k++] = right[j++];
         }
     }
+
 }
